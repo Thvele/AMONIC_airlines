79a0f9ad5848d96442de8f7c173d9343fbfaeabb
Thvele <thvele@mail.ru>
2023-03-04 16:44:18 +0300

    init

---
+++ AMONIC_airlines/AdminPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Windows;
+
+namespace AMONIC_airlines
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Window
+    {
+        SqlConnection connection = new SqlConnection((string)App.Current.Resources["connectionString"]);
+
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow authorization = new MainWindow();
+            authorization.Show();
+
+            this.Close();
+        }
+    }
+}
---
+++ AMONIC_airlines/Authorization.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace AMONIC_airlines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            AdminPage adminPage = new AdminPage();
+            adminPage.Show();
+
+            this.Close();
+        }
+    }
+}
